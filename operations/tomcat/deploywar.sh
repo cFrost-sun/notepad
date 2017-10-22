b367b9ef379e106505443ef344fc6d44a3b62099
cFrost <cFrost@eulerproject.io>
2017-10-22 21:51:13 +0800

    deploy

---
+++ operations/tomcat/deploywar.sh
@@ -0,0 +1,65 @@
+datetime=$(date +%Y-%m-%dT%H:%M:%S)
+deploy_tmp_dir=/root/cfrost/deploy_tmp
+deploy_dir=/app/tomcat/deploy
+webappname=tomcat7
+bak_dir=/app/tomcat/deploy/bak
+
+ueditor=false
+war_url=http://localhost
+war=test.war
+
+while getopts "n:ua:w:" arg #选项后面的冒号表示该选项需要参数
+do
+    case $arg in
+        n)
+            webappname=$OPTARG
+            ;;
+        u)
+            ueditor=true
+            ;;
+        a)
+            war_url=$OPTARG
+            ;;
+        w)
+            war=$OPTARG
+            ;;
+	esac
+done
+
+if [ ! -d "$deploy_tmp_dir" ]; then  
+    mkdir "$deploy_tmp_dir"
+fi
+
+if [ ! -d "$bak_dir" ]; then  
+    mkdir "$bak_dir"
+fi
+
+#清空下载临时目录
+rm -rf $deploy_tmp_dir/*
+
+#备份
+mkdir -p $bak_dir/$webappname/$datetime
+cp -rp $deploy_dir/$webappname/* $bak_dir/$webappname/$datetime
+
+#下载新包
+wget $war_url/$war -P $deploy_tmp_dir
+
+#停止tomcat
+service tomcat7 stop
+sleep 5
+
+#清空部署目录
+rm -rf $deploy_dir/$webappname/*
+
+#部署新文件
+unzip $deploy_tmp_dir/$war -d $deploy_dir/$webappname/
+
+#恢复UE文件
+if [ "$ueditor" == "true" ]; then
+    cp -rp $bak_dir/$webappname/$datetime/ueditor $deploy_dir/$webappname/ueditor
+fi
+
+#启动服务
+service tomcat7 start
+
+#~/deploy.sh -a https://ci.eulerproject.io/view/uwin-all/job/uwin-design-award/lastSuccessfulBuild/artifact/server/sz-design-award/target/ -w design-0.0.1-SNAPSHOT-debug.war -n tomcat7
